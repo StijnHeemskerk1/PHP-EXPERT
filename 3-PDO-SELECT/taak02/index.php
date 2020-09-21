2d5184b83d96f098cc2e457b4855b092789c465f
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-09-21 17:48:49 +0200

    3. taak2

---
+++ 3-PDO-SELECT/taak02/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $database_connectie = new PDO("mysql:host=localhost;dbname=toolsforever", "root", ""); //Connect dit aan de database
+
+    $sql = "SELECT * FROM locations"; //De mysql command in een variable
+    $statement = $database_connectie->prepare($sql); //Linked de command aan de connected database en zet dit in een variable
+    $statement->execute(); //Activeerd de command in de variable
+    $database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC); //Pakt alle gegevens en saved alle in een variable
+
+    foreach($database_gegevens as $gebruiker){  
+        echo $gebruiker['id'] . "<br>";
+        echo $gebruiker['name'] . "<br>";
+        echo $gebruiker['address'] . "<br>";
+        echo $gebruiker['city'] . "<br>";
+
+    }
+
+
+
+
+
+?>
