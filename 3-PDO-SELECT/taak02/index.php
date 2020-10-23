34edc3f00f56ad1503f64b47e831512799b517bf
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-10-23 14:31:36 +0200

    3. taak02 af

--- 3-PDO-SELECT/taak02/index.php
+++ 3-PDO-SELECT/taak02/index.php
@@ -1,19 +1,10 @@
 <?php
-
-    $database_connectie = new PDO("mysql:host=localhost;dbname=toolsforever", "root", ""); //Connect dit aan de database
-    
-    // public PDO::query("SELECT * FROM locations");
-
-    $sql = "SELECT * FROM locations"; //De mysql command in een variable
-    $statement = $database_connectie->prepare($sql); //Linked de command aan de connected database en zet dit in een variable
-    $statement->execute(); //Activeerd de command in de variable
-    $database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC); //Pakt alle gegevens en saved alle in een variable
-
-    foreach($database_gegevens as $gebruiker){  
-        echo $gebruiker['id'] . "<br>";
-        echo $gebruiker['name'] . "<br>";
-        echo $gebruiker['address'] . "<br>";
-        echo $gebruiker['city'] . "<br>";
+     $conn = new PDO("mysql:host=localhost;dbname=toolsforever", "root", "");
+     $sql = "SELECT * FROM locations";
+     foreach ($conn->query($sql) as $row){
+        print $row['id'] . ', ';
+        print $row['name'] . ', ';
+        print $row['address'] . ', ';
+        print $row['city'] . '<br>';
     }
-
 ?>
